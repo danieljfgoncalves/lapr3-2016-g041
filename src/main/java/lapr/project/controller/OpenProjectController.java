61e7a19b2e6a90f49103b4e386f9ad8c6989c33a
João Pereira <1151241@isep.ipp.pt>
2016-12-15 17:41:59 +0000

    Refractors Open Project Controller [controller][refractor][issue #9]

---
+++ src/main/java/lapr/project/controller/OpenProjectController.java
@@ -0,0 +1,30 @@
+/*
+ * Package location for controllers
+ */
+package lapr.project.controller;
+
+import lapr.project.model.Project;
+import lapr.project.model.Simulator;
+
+/**
+ * Controller responsible for opening projects
+ *
+ * @author Daniel Gonçalves - 1151452
+ * @author Eric Amaral - 1141570
+ * @author Ivo Ferro - 1151159
+ * @author João Pereira - 1151241
+ * @author Tiago Correia - 1151031
+ */
+public class OpenProjectController {
+    
+    Simulator simulator;
+    
+    public OpenProjectController(Simulator simulator){
+      this.simulator = simulator;        
+    }
+    
+    public void activeProjects(Project project){
+        simulator.setActivatedProject(project); 
+    }
+}
+
