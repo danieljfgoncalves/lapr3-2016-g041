48972185120c5e3522562e6526d62fdf659bef9a
ivoferroo <1151159@isep.ipp.pt>
2016-12-20 18:29:08 +0000

    implements import aircraft models [ui][implementation][UC-5][US-p05][fixes issue #17]

---
+++ src/main/java/lapr/project/ui/ImportAircraftModelsUI.java
@@ -0,0 +1,65 @@
+/**
+ * Package location for UI classes.
+ */
+package lapr.project.ui;
+
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import javax.xml.parsers.ParserConfigurationException;
+import lapr.project.controller.ImportAircraftModelsController;
+import lapr.project.model.Project;
+import lapr.project.ui.components.ImportFileChooser;
+import org.xml.sax.SAXException;
+
+/**
+ * Import Air Network UI.
+ *
+ * @author Daniel Gonçalves - 1151452
+ * @author Eric Amaral - 1141570
+ * @author Ivo Ferro - 1151159
+ * @author João Pereira - 1151241
+ * @author Tiago Correia - 1151031
+ */
+public class ImportAircraftModelsUI extends ImportFileChooser {
+
+    /**
+     * Selected Project.
+     */
+    private final Project selectedProject;
+
+    /**
+     * Creates an instance of the custom file chooser.
+     *
+     * @param project
+     */
+    public ImportAircraftModelsUI(Project project) {
+
+        this.selectedProject = project;
+    }
+
+    @Override
+    public void approveSelection() {
+        super.approveSelection();
+
+        // Create import controller
+        ImportAircraftModelsController controller = new ImportAircraftModelsController(selectedProject);
+
+        try {
+            // Import selected File
+            controller.importFile(getSelectedFile());
+            // If no critical error
+            JOptionPane.showMessageDialog(this.getParent(),
+                    "The air network was successfully added!",
+                    "Import Successful",
+                    JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (SAXException | IOException | NumberFormatException | ParserConfigurationException ex) {
+
+            JOptionPane.showMessageDialog(this,
+                    String.format("Invalid File!%nPlease select a different file.%n(Error:%s)", ex.getMessage()),
+                    "Invalid file",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+}
