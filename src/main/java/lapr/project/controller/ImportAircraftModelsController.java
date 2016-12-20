e71ac270f2e7bb98307881ed0f89bac9c4f72537
ivoferroo <1151159@isep.ipp.pt>
2016-12-20 17:57:18 +0000

    implements import aircraft models controller [controller][implementation][UC-5][US-p05][issue #17]

---
+++ src/main/java/lapr/project/controller/ImportAircraftModelsController.java
@@ -0,0 +1,51 @@
+/**
+ * Package location for Apllication Controllers concepts.
+ */
+package lapr.project.controller;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import lapr.project.model.AircraftModelsRegister;
+import lapr.project.model.Project;
+import org.xml.sax.SAXException;
+
+/**
+ * Controller to create a project.
+ *
+ * @author Eric Amaral 1141570
+ * @author Daniel Gonçalves 1151452
+ * @author Ivo Ferro 1151159
+ * @author Tiago Correia 1151031
+ */
+public class ImportAircraftModelsController {
+
+    /**
+     * The project.
+     */
+    private final Project project;
+
+    /**
+     * Creates an instance of ImportAircraftModelsController.
+     *
+     * @param project project to be imported
+     */
+    public ImportAircraftModelsController(Project project) {
+        this.project = project;
+    }
+
+    /**
+     * Imports a given XML file.
+     *
+     * @param file file to import
+     * @return true if it is successfully imported, false otherwise
+     * @throws SAXException parser error
+     * @throws IOException invalid file or path
+     * @throws ParserConfigurationException configurations errors
+     */
+    public boolean importFile(File file) throws SAXException, IOException, ParserConfigurationException {
+        AircraftModelsRegister aircraftModelsRegister = project.getAircraftModelsRegister();
+        return aircraftModelsRegister.importXml(file);
+    }
+
+}
