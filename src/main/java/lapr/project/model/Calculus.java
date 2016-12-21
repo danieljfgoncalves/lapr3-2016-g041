6f154f5bcbc608bc67412b60e16ac8434dcea5d5
ivoferroo <1151159@isep.ipp.pt>
2016-12-21 20:28:19 +0000

    creates calculus class [model][implementation]

---
+++ src/main/java/lapr/project/model/Calculus.java
@@ -0,0 +1,18 @@
+/**
+ * Package location for Model concepts.
+ */
+package lapr.project.model;
+
+/**
+ * Resposible to make calculations related to model business.
+ *
+ * @author Daniel Gonçalves - 1151452
+ * @author Eric Amaral - 1141570
+ * @author Ivo Ferro - 1151159
+ * @author João Pereira - 1151241
+ * @author Tiago Correia - 1151031
+ */
+public class Calculus {
+
+    // TODO implement static methods.
+}
