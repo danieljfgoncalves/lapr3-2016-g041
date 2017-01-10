a2dc204748bb40f99b13647e95e0bd81a1e595fa
ivoferroo <1151159@isep.ipp.pt>
2017-01-10 16:03:18 +0000

    improves simulate flight dialog [ui][refactor][US-N02][issue #39]

--- src/main/java/lapr/project/ui/components/ListModelProject.java
+++ src/main/java/lapr/project/ui/components/ListModelProject.java
@@ -13,7 +13,6 @@ import lapr.project.model.Project;
  * @author Daniel Gonçalves - 1151452
  * @author Eric Amaral - 1141570
  * @author Ivo Ferro - 1151159
- * @author João Pereira - 1151241
  * @author Tiago Correia - 1151031
  */
 public class ListModelProject extends AbstractListModel<Project> {
